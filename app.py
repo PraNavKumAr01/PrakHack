71fb46f89d05c46703d6dbecccf9cc7e9c58eeeb
Pranav Kumar <96071514+PraNavKumAr01@users.noreply.github.com>
2023-10-04 21:18:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,3 +68,6 @@ async def generate_text(data: InputData):
 
     return response
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
